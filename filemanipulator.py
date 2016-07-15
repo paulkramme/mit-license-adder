1865413c1932a3108883dc2b77c67608d56be275
Paul Kramme <pjkramme@gmail.com>
2016-07-14 22:14:41 -0400

    Add fullname and date

--- filemanipulator.py
+++ filemanipulator.py
@@ -7,7 +7,7 @@ mit_license = ("""\
 /*
 MIT License
 
-Copyright (c) [year] [fullname]
+Copyright (c) 2016 Paul Kramme
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
